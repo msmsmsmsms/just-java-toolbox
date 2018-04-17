1370e77e5b8d49652cf9aa79f85d770e5b42a431
Jan Burkhardt <bjrke@users.noreply.github.com>
2018-04-17 10:18:40 +0200

    add a collector to create a table from an EntryStream and a value function (#20)

--- just-java-toolbox/src/main/java/de/justsoftware/toolbox/stream/EntryCollectors.java
+++ just-java-toolbox/src/main/java/de/justsoftware/toolbox/stream/EntryCollectors.java
@@ -1,18 +1,22 @@
 package de.justsoftware.toolbox.stream;
 
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSetMultimap;
+import com.google.common.collect.ImmutableTable;
 
 /**
  * Collectors for streams of {@link java.util.Map.Entry}s. Handy usable with {@link EntryStream}.
  */
+@ParametersAreNonnullByDefault
 public class EntryCollectors {
 
     @Nonnull
@@ -33,6 +37,13 @@ public class EntryCollectors {
     @Nonnull
     public static <K, V> Collector<Map.Entry<K, V>, ?, ImmutableListMultimap<K, V>> toImmutableListMultimap() {
         return ImmutableListMultimap.toImmutableListMultimap(Map.Entry::getKey, Map.Entry::getValue);
+    }
+
+    @Nonnull
+    public static <R, C, V> Collector<Map.Entry<R, C>, ?, ImmutableTable<R, C, V>> toImmutableTable(
+            final BiFunction<R, C, V> valueFunction) {
+        return ImmutableTable.toImmutableTable(Map.Entry::getKey, Map.Entry::getValue,
+                e -> valueFunction.apply(e.getKey(), e.getValue()));
     }
 
 }
